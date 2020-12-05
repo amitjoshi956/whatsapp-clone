6fa1b43a42eecae7d946e2016247ffe6a49dda98
Amit Joshi <amitjoshi956@gmail.com>
2020-12-06 00:46:51 +0530

    Converting to css modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import * as classes from './App.module.css';
 import Sidebar from './components/Sidebar/Sidebar'
 import Chat from './components/Chat/Chat';
 
 function App() {
   return (
-    <div className="app">
-      <div className="app_body">
+    <div className={classes.App}>
+      <div className={classes.AppBody}>
         <Sidebar />
         <Chat />
       </div>
